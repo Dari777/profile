fbf5870d246f219c75af366dfe8d5e921fd50d56
Dari777 <70533479+Dari777@users.noreply.github.com>
2022-10-23 22:32:05 +0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,36 @@
+// scroller-top
+
+$(document).ready(function () {
+    $('#scroller').hide();
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('#scroller').fadeIn();
+        } else {
+            $('#scroller').fadeOut();
+        }
+    });
+    $('#scroller').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 300);
+        return false;
+    });
+});
+
+// tabs-panel
+
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
